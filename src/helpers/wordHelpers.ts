3d25793e85a8605b7476c3a4325b259c54f17802
Jeff McAmmond <jeffmcammond@gmail.com>
2021-05-23 21:01:14 -0600

    Easier number generator

--- src/helpers/wordHelpers.ts
+++ src/helpers/wordHelpers.ts
@@ -2,22 +2,6 @@ import WordModel from '../models/WordModel';
 import { SimpleList } from '../resources/wordList';
 import { AdvancedList } from '../resources/wordList'; 
 
-const shuffleArray = (array: any[]) => {
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-const getNumbersArray = (advanced: boolean) => {
-  const size = advanced ? 8425 : 123;
-  const splice = advanced ? 200 : 70;
-
-  let nums = Array.from({length: size}, (_, i) => i + 1 + "");
-  shuffleArray(nums);
-  return nums.splice(0, splice);
-}
-
 const probability = (n: number) => {
   return !!n && Math.random() <= n;
 };
@@ -45,15 +29,14 @@ export const randomWordGenerator = ({
     list.push(...AdvancedList);
   }
 
-  if (numbers) {
-    list.push(...getNumbersArray(advanced));
-  }
-
   while (wordsLength <= maxLength && attempts <= maxAttempts) {
     const randomInt = Math.floor(Math.random() * list.length);
     let word = list[randomInt];
 
-    if (punctuation && probability(.05) && words[words.length - 1] !== '-' && words.length !== 0) {
+    if (numbers && probability(.05)) {
+      const size = advanced ? 8425 : 123;
+      word = Math.floor(Math.random() * size) + '';
+    } else if (punctuation && probability(.05) && words[words.length - 1] !== '-' && words.length !== 0) {
       word = '-';
     } else if (punctuation && isProperWord(word) && probability(.1)) {
       word = '"' + word + '"';
